a8df9aa97a5321a0e6a3a9b7603eb8eaebd7ad7a
mohmmadAyesh <mohmmadayesh22@gmail.com>
2026-02-28 15:34:55 +0200

    add the multer logic so i can handle multipart form data

--- backend/routers/routes.js
+++ backend/routers/routes.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req,file,cb){
+        cb(null, 'uploads');
+    },
+    filename: function (req,file,cb){
+        cb(null, file.originalname);
+    }
+});
+const uploads = multer({storage});
 const router = express.Router();
 const { PostgresSQL } = require('../db/db');
 const { uploadImage, getAllImages } = require('../controllers/imageController');
 
-router.post('/upload', PostgresSQL, uploadImage);
+router.post('/upload',uploads.single('image'), PostgresSQL, uploadImage);
 router.get('/images', PostgresSQL, getAllImages);
 
 module.exports = router;
