561cd0d14adca213f8577d9be3055f63ed9d5c54
mohmmadAyesh <mohmmadayesh22@gmail.com>
2026-02-28 10:06:11 +0200

    add the file size validation and lightbox logic and styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css'
-import { useState } from 'react'
-import { ImageUpload } from './ImageUpload'
+import { useState } from 'react';
+import { ImageUpload } from './ImageUpload';
+const MAX_FILE_SIZE  = 1000 * 1000 * 3;
 function App() {
   const [galleryItems,setGallaryItems] = useState<string[]>([]);
-
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      if (file.size > MAX_FILE_SIZE) {
+        setErrorMessage("File size exceeds 5MB limit");
+        return;
+      }
       const reader = new FileReader()
       reader.onload = (event) => {
         const newImage = event.target?.result as string;
+        setUploadedImage(newImage);
         setGallaryItems(prevItems => [...prevItems, newImage]);
       }
       reader.readAsDataURL(e.target.files[0])
@@ -17,7 +25,7 @@ function App() {
 
   return (
     <>
-     <ImageUpload handleImageChange={handleImageChange} />
+     <ImageUpload handleImageChange={handleImageChange} errorMessage={errorMessage} uploadedImage={uploadedImage} />
      
     </>
   )
