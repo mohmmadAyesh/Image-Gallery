757266684169cc61aa574b5208aeffa79c083722
mohmmadAyesh <mohmmadayesh22@gmail.com>
2026-02-28 10:36:58 +0200

    fix add the postgresql dependecy fix controller file

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,4 +1,4 @@
-uploadImage = async (req, res) => {
+const uploadImage = async (req, res) => {
     try {
         const { filename, upload_date } = req.body;
         const image_s3_url = req.file;
@@ -11,3 +11,14 @@ uploadImage = async (req, res) => {
         res.status(500).json({ error: 'Failed to upload image' });
     }
 }
+const getAllImages = async (req,res) => {
+    try{
+        const query = 'SELECT * FROM images ORDER BY upload_date DESC';
+        const result = await req.pool.query(query);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error fetching images:', error);
+        res.status(500).json({ error: 'Failed to fetch images' });
+    }
+}
+module.exports = {uploadImage, getAllImages};
