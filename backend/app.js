da5ff204f658b59411d73d0e0df388abc8553999
mohmmadAyesh <mohmmadayesh22@gmail.com>
2026-03-01 16:41:53 +0200

    unify adding custom error class with handling it inside app.js

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const routes = require("./routers/routes");
 const messages = require("./services/error-handler");
+const e = require("express");
 app.use(cors());
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -17,9 +19,21 @@ app.use(PostgresSQL);
 app.use("/", routes);
 app.use((err, req, res, _next) => {
   let error = err;
-  const statusCode = error.statusCode || 500;
-  const errorMessage = messages[error.message] || "Internal Server Error";
-  res.status(statusCode).json({
+  let errorMessage = "";
+  let errorStatusCode = 500;
+  if (error.message === "File too large") {
+    errorMessage = messages["FILE_IS_LARGE"];
+    errorStatusCode = 400;
+  } else {
+    errorMessage = messages[error.message] || "Internal Server Error";
+    errorStatusCode = error.statusCode || 500;
+  }
+
+  console.log("Error in error handling middleware:", error);
+  console.log("Check if there is error status code:", error.statusCode);
+  console.log("Check if there is error message:", error.message);
+
+  res.status(errorStatusCode).json({
     error: errorMessage,
   });
 });
--- backend/services/error-handler/messages.js
+++ backend/services/error-handler/messages.js
@@ -1,4 +1,6 @@
 const errors = {
   FILE_IS_LARGE: "File exceeds the size limit",
+  FILE_TYPE_NOT_ALLOWED:
+    "File type not allowed consider uploading only png, jpg,jpeg gif only",
 };
 module.exports = errors;
